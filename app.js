6e55db9bee92e3516282f667a40e3f5a850dd2d5
Thomas Dulka <tjdulka@us.ibm.com>
2016-12-21 16:08:50 -0600

    New source

--- app.js
+++ app.js
@@ -1,28 +1,54 @@
-/*eslint-env node*/
+/**
+ * Copyright 2015 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
+const Prism = require('node-prismjs');
+const express = require('express');
+const app = express();
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
+const Fetcher = require('./discovery-client');
+const adnFetch = new Fetcher();
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+// Bootstrap application settings
+require('./config/express')(app);
 
-// create a new express server
-var app = express();
-
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+app.get('/', (req, res) => {
+  res.render('index');
 });
+
+app.get('/api/query/:query', (req, res, next) => {
+  adnFetch.fetchPompipeData(req.params.query, (error, statusCode, headers, body) => {
+    if (error || statusCode !== 200) {
+      return next(error);
+    }
+    try {
+      const results = { path: adnFetch.queryPath(req.params.query) };
+      results.raw = JSON.parse(body);
+      results.text = JSON.stringify(results.raw, null, 4);
+      const jsonString = JSON.stringify(results.raw, null, 2);
+      const prismOptions = Prism.languages.js;
+      results.highlighted = Prism.highlight(jsonString, prismOptions);
+      results.fields = adnFetch.fieldParams(req.params.query);
+      return res.json(results);
+    } catch (e) {
+      return next(e);
+    }
+  });
+});
+
+// error-handler settings
+require('./config/error-handler')(app);
+
+module.exports = app;
